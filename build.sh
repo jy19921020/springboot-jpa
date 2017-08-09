0fc0b9719bda912aa657554b571a7ba3ae2cbc17
jy19921020 <691096060@qq.com>
2017-08-09 14:23:07 +0800

    修改为本地0.1

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 source /etc/profile
 
-MYIMAGE=192.168.1.2:8082/springboot/springboot-jpa
+MYIMAGE=127.0.0.1:8082/springboot/springboot-jpa
 
 # uncomment if you need push
 #docker login 192.168.1.2:8082 -u admin -p admin123
 
 # stop all container
-docker kill $(docker ps -aq)
+#docker kill $(docker ps -aq)
 
 # remove all container
-docker rm $(docker ps -aq)
+#docker rm $(docker ps -aq)
 
 # remove old images
-docker images | grep 192.168.1.2:8082/springboot/springboot-jpa | awk '{print $3}' | xargs docker rmi
+#docker images | grep 192.168.1.2:8082/springboot/springboot-jpa | awk '{print $3}' | xargs docker rmi
 
 # build jar and image
 mvn package -e -X docker:build -DskipTest
